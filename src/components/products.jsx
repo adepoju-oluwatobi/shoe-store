c07525efe22d7530a38c2d95f441ee832641b749
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-06-06 17:46:35 +0100

    modified the products

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -4,8 +4,8 @@ import ProductCard from './ProductCard'
 function products({products}) {
   return (
   <div>
-      <div>
-        <h1>Products</h1>
+      <div className='px-4 -mb-3 mt-8'>
+        <h1 className='heading'>Products</h1>
       </div>
       <div className='p-4 w-[100%] h-[100%] grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-2 m-auto'>
        {products.map((item) => (
